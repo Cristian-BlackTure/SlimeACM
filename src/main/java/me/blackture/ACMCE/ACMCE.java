90bd1ec8de68851e47b6e9caa188528b3e6111c0
Cristian-BlackTure <110348555+Cristian-BlackTure@users.noreply.github.com>
2022-10-06 00:25:40 +0300

    Update ACMCE.java

--- src/main/java/me/blackture/ACMCE/ACMCE.java
+++ src/main/java/me/blackture/ACMCE/ACMCE.java
@@ -5,9 +5,7 @@ import ACMItems;
 public class ACMCE extends JavaPlugin implements ACMCE {
 
 		@Override
-
 	public void onEnable() {
-
 		// ...
 
 	}
@@ -15,9 +13,7 @@ public class ACMCE extends JavaPlugin implements ACMCE {
 	
 
 	@Override
-
 	public void onDisable() {
-
 		// Logic for disabling the plugin...
 
 	}
@@ -27,11 +23,8 @@ public class ACMCE extends JavaPlugin implements ACMCE {
 	@Override
 
 	public JavaPlugin getJavaPlugin() {
-
 		// This is a method that links your SlimefunAddon to your Plugin.
-
 		// Just return "this" in this case, so they are linked
-
 		return this;
 
 	}
@@ -39,21 +32,13 @@ public class ACMCE extends JavaPlugin implements ACMCE {
 	
 
 	@Override
-
 	public String getBugTrackerURL() {
-
 		// Here you can return a link to your Bug Tracker.
-
 		// This link will be displayed to Server Owners if there is an issue
-
 		// with this Addon. Return null if you have no bug tracker.
-
 		// Normally you can just use GitHub's Issues tab:
-
 		// https://github.com/YOURNAME/YOURPROJECT/issues
-
 		return null;
-
 	}
 
 }
